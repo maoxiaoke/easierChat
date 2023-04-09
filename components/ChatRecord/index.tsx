2171ef0aec2722206676cad5cae09e379e0f115b
maoxiaoke <maoxiaoke@outlook.com>
2023-04-09 23:00:51 +0800

    add throttle

--- components/ChatRecord/index.tsx
+++ components/ChatRecord/index.tsx
@@ -1,5 +1,6 @@
 import { memo } from 'react';
 import * as Avatar from '@radix-ui/react-avatar';
+import { useThrottle } from 'ahooks';
 import cls from 'classnames';
 
 export interface ChatRecordProps {
@@ -21,24 +22,30 @@ export const AvatarComponent = ({ role }: { role: ChatMessage['role'] }) => (
 
 const AvatarMemo = memo(AvatarComponent);
 
-const ChatRecord = ({ chats = [] } : ChatRecordProps) => {
+const ChatRecords = ({ chats = [] } : ChatRecordProps) => {
   return (
     <div>
       { chats.map((chat) => (
-        <div
-          key={chat.id ?? chat.date}
-          className="flex items-start px-2 relative response-block scroll-mt-32 rounded-md hover:bg-gray-50 dark:hover:bg-zinc-900 pb-2 pt-2 pr-2 group min-h-[52px]"
-        >
-          <AvatarMemo role={chat.role} />
-          <div className={cls("ml-3 text-sm whitespace-pre-line focus:outline", chat.role === 'user' && 'bg-blue-500 text-white px-4 py-2 rounded-lg')}>
-            { chat.text }
-          </div>
-        </div>
+        <ChatRecord key={chat.id ?? chat.date} chat={chat} />
       ))}
     </div>
   )
 }
 
+const ChatRecord = ({ chat }: { chat: ChatMessage }) => {
+  const text  = useThrottle(chat.text, { wait: 200 })
+  return (
+    <div
+      className="flex items-start px-2 relative response-block scroll-mt-32 rounded-md hover:bg-gray-50 dark:hover:bg-zinc-900 pb-2 pt-2 pr-2 group min-h-[52px]"
+    >
+      <AvatarMemo role={chat.role} />
+      <div className={cls("ml-3 text-sm whitespace-pre-line focus:outline", chat.role === 'user' && 'bg-blue-500 text-white px-4 py-2 rounded-lg')}>
+        { text }
+      </div>
+    </div>
+  )
+
+}
 
 
-export default ChatRecord;
+export default ChatRecords;
