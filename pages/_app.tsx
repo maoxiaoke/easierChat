cf794486e531a236f55f546808afea281c159355
maoxiaoke <maoxiaoke@outlook.com>
2023-04-09 10:54:06 +0800

    add analysis

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,8 +3,16 @@ import '@/styles/modal.css';
 import '@/styles/avatar.css';
 import '@/styles/dailog.css';
 import '@/styles/context.css';
+
+import { Analytics } from '@vercel/analytics/react';
+
 import type { AppProps } from 'next/app'
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Component {...pageProps} />
+      <Analytics />
+    </>
+  )
 }
