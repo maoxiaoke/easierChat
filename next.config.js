53631cd5c2361679a51bd26f6f0218b94334ac5f
maoxiaoke <maoxiaoke@outlook.com>
2023-05-13 13:08:33 +0800

    feat: pwa

--- next.config.js
+++ next.config.js
@@ -1,6 +1,16 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-}
+const runtimeCaching = require("next-pwa/cache");
+
+const withPWA = require("next-pwa")({
+  dest: "public",
+  register: true,
+  skipWaiting: true,
+  runtimeCaching,
+  buildExcludes: [/middleware-manifest.json$/]
+});
+
+const nextConfig = withPWA({
+  reactStrictMode: true
+})
 
 module.exports = nextConfig
